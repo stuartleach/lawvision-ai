d17c5eeeb2a2e294f93d6851bee65924fab96944
Stuart Leach <jstuartleach@gmail.com>
2024-12-07 19:13:35 -0500

    fix errors

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,6 +1,6 @@
 export const About = () => {
     return (
-        <div className="px-6 pb-32 pt-16 lg:px-8 font-light">
+        <div className="px-6 pb-32 pt-16 lg:px-8 font-light tracking-tight">
             <div className="mx-auto max-w-3xl text-base/7 text-zinc-300">
                 {/* <p className="text-base/7 font-semibold text-indigo-500">Introducing</p> */}
                 <h1 className="mt-2 text-pretty text-4xl font-semibold tracking-tight text-zinc-300 sm:text-5xl border-b border-white border-opacity-40 pb-2 mb-4">
@@ -53,11 +53,51 @@ export const About = () => {
                         Assign profiles to every judge in the state, comparing them to judges in their county and state.
                     </li>
                 </ol>
+                <h2 className="mt-6 text-3xl font-semibold tracking-tight text-zinc-100">Research Questions</h2>
+
+                <ul className="my-6 list-disc space-y-2 text-xl/8">
+                    <li>
+                        Are there factors that have an correlation on the amount of bail set?
+                    </li>
+                    <li>
+                        What factors have a high importance value that shouldn't, e.g., race, gender, median income of the county?
+                    </li>
+                    <li>
+                        What factors have a higher importance value to some judges than others?
+                    </li>
+                </ul>
 
 
 
+                <p className="mt-6 text-xl/8 text-purple-500 font-semibold">
+                    Drawing inferences
+                </p>
 
-{/* 
+                <p className="text-xl/8 text-purple-300">
+                    Correlation ≠ Causation. This model is good at noticing patterns, but responsible inferences shouldn't take us further than appreciating correlations between datapoints—i.e. just because a judge's bail amounts reflect a strikingly high race-importance, for example, we ought not to assume race plays into the bail decisions. This model is more interested in questions than answers, and is certainly not meant to tar-and-feather anyone in the justice system.
+                </p>
+
+
+
+                <p className="mt-6 text-xl/8 text-red-500 font-semibold">
+                    Disclaimer
+                </p>
+                <p className="text-xl/8 text-red-300">
+                    This is not and will never be a tool to be used by counsel. It can't teach a lawyer what to emphasize on their client's behalf. This model will never be as effective as a reasonably perceptive defense attorney. This project is only to understand the extent that judges in New York State abide by the rules laid out in CPL 510.10.
+                </p>
+
+
+
+                <p className="mt-6 text-yellow-300 text-xl/8 font-semibold">
+                    Incompleteness of the dataset
+                </p>
+
+                <p className="text-yellow-100 text-xl/8">
+                    This data is incomplete, both in its range and its scope. In a perfect world (or one where compute is cheaper and court records are more thorough), this model would take bail eligibility into account, and set ROR "release on recognizance" decisions to a bail amount of $0.00, which would give us deeper and more complete insight into a correlations between bail amounts and other factors. As it is currently running, the model only accounts for bail amounts > 0.
+                </p>
+
+
+                {/* 
 
                 <div className="font-light tracking-tight">
                 </div>
